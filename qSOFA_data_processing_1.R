5b4df0c730ea335be71b01cda2f0c689976ea9cc
bjornlindbergKI <70630935+bjornlindbergKI@users.noreply.github.com>
2020-10-12 13:12:00 +0200

    Code table1 and introduction from progressreport KI

--- qSOFA_data_processing_1.R
+++ qSOFA_data_processing_1.R
@@ -4,10 +4,15 @@ library(rio)
 library(assertthat)
 library(dplyr)
 library(rmarkdown)
-##function evaluation qSOFA on input.
+library(tableone)
+library(tinytex)
+library(DiagrammeR)
+library(networkD3)
+library(webshot)
 
-## This absolutely gets the job done. If you're interested in
-## writing it in a more R-ish way then this is a suggestion:
+
+
+##function evaluation qSOFA on input.
 qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
     ## Check arguments
     assert_that(is.data.frame(raw))
@@ -28,31 +33,38 @@ qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
 }
 
 #Data extraction
+
+
 url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-limited-dataset-v1.csv"
+#url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-full-dataset-v1.csv"
 tot_data <- rio::import(url)
-#some of the data I though could be interesting
-part_data <- subset(tot_data, select = c(age, sex, tran, moi, sbp_1, rr_1, gcs_t_1, sbp_2, rr_2, gcs_t_2, licu, iss, died))
-time_data <- subset(tot_data, select = c(doar, doa, dodd, dom_1, dom_2, toar, toa, todd, tom_1, tom_2))
-## found two messurments, if incomplete, should we combined them?
-## We can use either but not mix. So if the second measurements are more complete then we can use those.
+
 qSOFA_1raw <- tot_data[,c("sbp_1","rr_1","gcs_t_1")]
 qSOFA_2raw <- tot_data[,c("sbp_2","rr_2","gcs_t_2")]
 
-## not sure if we need all this data about the time or that this is the best way but i got carried away...
-
-## Probably not...
-days <- sapply(time_data[,c(1:5)], as.Date)
-days <- days - days[,"doar"]
-time <- time_data[,c(6:10)] 
-icu_hours <- data.frame(tot_data[,"licu"])
-names(icu_hours) <- paste("icu_hours")
-icu_days <- icu_hours/24
-names(icu_days) <- paste("icu_days")
-icu_bin <- data.frame(as.integer(icu_days>0))
-names(icu_bin) <- paste("icu_bin") # binary value of ICU admission
 
 qSOFA_1 <- qSOFAcalc(qSOFA_1raw)
 qSOFA_2 <- qSOFAcalc(qSOFA_2raw)
+
+
+# Part data and as factor #### 
+#some of the data I though could be interesting
+part_data = subset(tot_data, select = c(incl,age, sex, tran, moi, licu, died,sbp_1,rr_1,gcs_t_1))
+
+
+# age as number,  > 89 was changed with 90 instead of NA
+part_data$age <- as.numeric(part_data$age)
+part_data$age[is.na(part_data$age)] <- 90
+# sex, mode of injury as factor
+part_data$sex <- as.factor(part_data$sex)
+part_data$moi <- as.factor(part_data$moi)
+part_data$died <- as.factor(part_data$died)
+part_data$incl <- as.factor(part_data$incl)
+# ICU as binary
+part_data$licu[part_data$licu > 0] <- "Yes"
+part_data$licu[part_data$licu == 0] <- "No"
+
+## Martins lista #####
 
 ## Either create "global" objects that can be accessed directly from
 ## your R mardkown file or store everything you want to include there
@@ -66,6 +78,87 @@ younger.than.18 <- part_data$age < 18
 results$n.younger.than.18 <- sum(younger.than.18)
 study.sample <- part_data[!younger.than.18, ]
 results$n.adults <- nrow(study.sample)
+
+## exclude Those who died before admission ie incl==2
+
+results$n.incl2 <- sum(study.sample$incl == 2)
+study.sample <- study.sample[!study.sample$incl == 2,]
+results$n.included <- nrow(study.sample)
+
+## exclude NA's
+results$n.NA_TOT <- sum(is.na(study.sample$rr_1)|is.na(study.sample$sbp_1)|is.na(study.sample$gcs_t_1)|is.na(study.sample$licu))
+results$n.NA_ICU <- sum(is.na(study.sample$licu))
+results$n.NA_SBP <- sum(is.na(study.sample$sbp_1))
+results$n.NA_RR <- sum(is.na(study.sample$rr_1))
+results$n.NA_GCS <- sum(is.na(study.sample$gcs_t_1))
+
+
+##delete all patients containing missing data in SBP,GCS or ICU
+study.sample.complete <- study.sample[!is.na(study.sample$rr_1)& !is.na(study.sample$sbp_1)& !is.na(study.sample$gcs_t_1)& !is.na(study.sample$licu),]
+
+results$n.complete <- nrow(study.sample.complete)
+
+## Table 1####
+
+data.table1 <- study.sample.complete
+data.table1$incl <- NULL
+
+## change names
+names(data.table1) <- c("Age", "Sex", "Transported", "Mode of injury", "Admitted to the ICU", "Died after admission", "SBP", "RR", "GCS")
+
+
+## combine road traffic injury to one category
+data.table1$`Mode of injury` <- as.character(data.table1$`Mode of injury`)
+RTI <- grepl('traffic', data.table1$`Mode of injury`)
+data.table1$`Mode of injury`[RTI] <- "Road traffic injury" 
+
+CreateTableOne(data =data.table1)
+
+## Figure 1 flowchart ####
+
+figure1 <- grViz("digraph flowchart {
+      # node definitions with substituted label text
+      node [fontname = Helvetica, shape = rectangle, width = 4]        
+      graph [rankdir = LR]
+      
+      
+      
+      tab1 [label = '@@1']
+      tab2 [label = '@@2']
+      tab3 [label = '@@3']
+      tab4 [label = '@@4']
+      tab5 [label = '@@5']
+      tab6 [label = '@@6']
+      tab7 [label = '@@7']
+      
+      subgraph {
+      rank = same;
+      tab1 -> tab2 -> tab3 -> tab4
+      
+      }
+      
+
+      # edge definitions with the node IDs
+      tab1 -> tab5 
+      tab2 -> tab6 
+      tab3 -> tab7
+      }
+
+      [1]: paste0('Participants in the TITCO cohort: ', results$n.cohort )
+      [2]: paste0('Participants above age 18: ', results$n.adults)
+      [3]: paste0('Participants alive at admission: ', results$n.included)
+      [4]: paste0('Participants with complete data: ', results$n.complete)
+      [5]: paste0('Participants below age 18: ', results$n.younger.than.18)
+      [6]: paste0('Participants died before admission: ', results$n.incl2)
+      [7]: paste0('Participants with missing data: ', results$n.NA_TOT, '\\n', 'Missing ICU admission: ', results$n.NA_ICU, '\\n','Missing systolic blood preassure: ', results$n.NA_SBP, '\\n', 'Missing respiratory rate: ', results$n.NA_RR, '\\n', 'Missing Glascow coma scale: ', results$n.NA_GCS)
+
+      ")
+
+
+saveNetwork(figure1, "figure1.html")
+
+
+webshot("figure1.html", "figure1.png")
 
 ## Compile paper
 render("study-plan.Rmd")
