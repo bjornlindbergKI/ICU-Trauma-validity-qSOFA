a27fafb7186392e2392c9c5d55383871004c81aa
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-10-13 21:16:05 +0200

    Edit code

--- qSOFA_data_processing_1.R
+++ qSOFA_data_processing_1.R
@@ -7,10 +7,11 @@ library(rmarkdown)
 library(tableone)
 library(tinytex)
 library(DiagrammeR)
+library(DiagrammeRsvg)
 library(networkD3)
 library(webshot)
-
-
+library(knitr)
+library(rsvg)
 
 ##function evaluation qSOFA on input.
 qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
@@ -33,34 +34,29 @@ qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
 }
 
 #Data extraction
-
-
 url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-limited-dataset-v1.csv"
 #url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-full-dataset-v1.csv"
 tot_data <- rio::import(url)
-
 qSOFA_1raw <- tot_data[,c("sbp_1","rr_1","gcs_t_1")]
 qSOFA_2raw <- tot_data[,c("sbp_2","rr_2","gcs_t_2")]
-
-
 qSOFA_1 <- qSOFAcalc(qSOFA_1raw)
 qSOFA_2 <- qSOFAcalc(qSOFA_2raw)
 
+## Part data and as factor some of the data I though could be
+## interesting
+part_data = subset(tot_data, select = c(incl, age, sex, tran, moi, licu, died, sbp_1, rr_1, gcs_t_1))
 
-# Part data and as factor #### 
-#some of the data I though could be interesting
-part_data = subset(tot_data, select = c(incl,age, sex, tran, moi, licu, died,sbp_1,rr_1,gcs_t_1))
-
-
-# age as number,  > 89 was changed with 90 instead of NA
+## age as number,  > 89 was changed with 90 instead of NA
 part_data$age <- as.numeric(part_data$age)
 part_data$age[is.na(part_data$age)] <- 90
-# sex, mode of injury as factor
+
+## sex, mode of injury as factor
 part_data$sex <- as.factor(part_data$sex)
 part_data$moi <- as.factor(part_data$moi)
 part_data$died <- as.factor(part_data$died)
 part_data$incl <- as.factor(part_data$incl)
-# ICU as binary
+
+## ICU as binary
 part_data$licu[part_data$licu > 0] <- "Yes"
 part_data$licu[part_data$licu == 0] <- "No"
 
@@ -79,8 +75,7 @@ results$n.younger.than.18 <- sum(younger.than.18)
 study.sample <- part_data[!younger.than.18, ]
 results$n.adults <- nrow(study.sample)
 
-## exclude Those who died before admission ie incl==2
-
+## exclude those who died before admission ie incl==2
 results$n.incl2 <- sum(study.sample$incl == 2)
 study.sample <- study.sample[!study.sample$incl == 2,]
 results$n.included <- nrow(study.sample)
@@ -92,36 +87,27 @@ results$n.NA_SBP <- sum(is.na(study.sample$sbp_1))
 results$n.NA_RR <- sum(is.na(study.sample$rr_1))
 results$n.NA_GCS <- sum(is.na(study.sample$gcs_t_1))
 
-
-##delete all patients containing missing data in SBP,GCS or ICU
+## delete all patients containing missing data in SBP, GCS or ICU
 study.sample.complete <- study.sample[!is.na(study.sample$rr_1)& !is.na(study.sample$sbp_1)& !is.na(study.sample$gcs_t_1)& !is.na(study.sample$licu),]
-
 results$n.complete <- nrow(study.sample.complete)
 
-## Table 1####
-
+## Table 1
 data.table1 <- study.sample.complete
 data.table1$incl <- NULL
 
 ## change names
 names(data.table1) <- c("Age", "Sex", "Transported", "Mode of injury", "Admitted to the ICU", "Died after admission", "SBP", "RR", "GCS")
 
-
 ## combine road traffic injury to one category
 data.table1$`Mode of injury` <- as.character(data.table1$`Mode of injury`)
 RTI <- grepl('traffic', data.table1$`Mode of injury`)
 data.table1$`Mode of injury`[RTI] <- "Road traffic injury" 
 
-CreateTableOne(data =data.table1)
-
-## Figure 1 flowchart ####
-
+## Figure 1 flowchart
 figure1 <- grViz("digraph flowchart {
       # node definitions with substituted label text
       node [fontname = Helvetica, shape = rectangle, width = 4]        
       graph [rankdir = LR]
-      
-      
       
       tab1 [label = '@@1']
       tab2 [label = '@@2']
@@ -154,11 +140,7 @@ figure1 <- grViz("digraph flowchart {
 
       ")
 
-
-saveNetwork(figure1, "figure1.html")
-
-
-webshot("figure1.html", "figure1.png")
+export_svg(figure1) %>% charToRaw %>% rsvg_svg("figure1.svg")
 
 ## Compile paper
 render("study-plan.Rmd")
