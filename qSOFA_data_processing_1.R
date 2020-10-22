ff43be4cdf1cf49f09390e284b0da2c9e6d4b0db
Martin Gerdin Warnberg <martingerdin@gmail.com>
2020-10-22 20:14:40 +0200

    Edit code

--- qSOFA_data_processing_1.R
+++ qSOFA_data_processing_1.R
@@ -13,7 +13,7 @@ library(webshot)
 library(knitr)
 library(rsvg)
 library("bengaltiger")
-
+library(ROCR)
 
 ##function evaluation qSOFA on input.
 qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
@@ -39,7 +39,7 @@ qSOFAcalc <- function(raw, tot = TRUE, sbp_cut = 100, rr_cut = 22) {
     score
 }
 
-#Data extraction
+# Data extraction
 url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-limited-dataset-v1.csv"
 #url <- "https://raw.githubusercontent.com/titco/titco-I/master/titco-I-full-dataset-v1.csv"
 tot_data <- rio::import(url)
@@ -53,8 +53,8 @@ qSOFA_2 <- qSOFAcalc(qSOFA_2raw)
 part_data = subset(tot_data, select = c(incl, age, sex, tran, moi, licu, died, sbp_1, rr_1, gcs_t_1))
 
 ## age as number,  > 89 was changed with 90 instead of NA
+part_data$age[part_data$age == ">89"] <- 90
 part_data$age <- as.numeric(part_data$age)
-part_data$age[is.na(part_data$age)] <- 90
 
 ## sex, mode of injury as factor
 part_data$sex <- as.factor(part_data$sex)
@@ -156,13 +156,12 @@ figure1 <- grViz("digraph flowchart {
 
 export_svg(figure1) %>% charToRaw %>% rsvg_svg("figure1.svg")
 
-## calculate qSOFA score and add to study.sample.complete ####
-qSOFA <- qSOFAcalc(study.sample.complete, tot=FALSE)
-study.sample.complete.sofa <- data.frame(study.sample.complete,qSOFA)
+## Calculate qSOFA score and add to study.sample.complete ####
+qSOFA <- qSOFAcalc(study.sample.complete, tot = FALSE)
+study.sample.complete.sofa <- data.frame(study.sample.complete, qSOFA)
 
 
-## split sample in sample.split #### 
-
+## Split sample in sample.split #### 
 proportion <- c(0.6,0.20,0.20)
 split.names <- c("training.sample","validation.sample", "test.sample")
 
@@ -171,35 +170,18 @@ sample.split <- SplitDataset(study.sample.complete.sofa, events = NULL, event.va
                              temporal.split = NULL, remove.missing = FALSE, random.seed = NULL,
                              sample.names = split.names, return.data.frame = FALSE)
 
-summary(sample.split$training.sample)
-summary(sample.split$validation.sample)
-summary(sample.split$test.sample)
-
 ## Training #### --------------------- NY
 training.sample <- sample.split$training.sample
-
-training.ICU <- as.numeric(training.sample$licu=="Yes")
-training.died <- as.numeric(training.sample$died=="Yes")
-
-x1 <- training.sample$sbp_score
-x2 <- training.sample$rr_score
-x3 <- training.sample$gcs_score
-y <- training.ICU
-yxxx <- data.frame(y , x1, x2, x3)
-
-fit <- glm(y ~., family=binomial(link='logit'), data=yxxx)
-
+fit <- glm(as.numeric(licu == "Yes") ~ sbp_score + rr_score + gcs_score, family = binomial, data = training.sample)
 
 ## ROCR curve
-library(ROCR)
-p <- predict(fit, newdata=subset(sample.split$training.sample,select=c(11,12,13)), type="response")
-pr <- prediction(p, as.numeric(sample.split$training.sample$licu=="Yes"))
+p <- predict(fit, newdata = training.sample, type="response")
+pr <- prediction(p, as.numeric(training.sample$licu == "Yes"))
 prf <- performance(pr, measure = "tpr", x.measure = "fpr")
 plot(prf)
 auc <- performance(pr, measure = "auc")
 auc <- auc@y.values[[1]]
-auc
-
+results$updated.auc <- auc
 
 ## Compile paper ####
 render("study-plan.Rmd")
